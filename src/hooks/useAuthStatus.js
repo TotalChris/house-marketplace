37e239a7ba28ea826494f3ac0ff42792742bd0b1
Chris Yates <chris.m.yates@outlook.com>
2023-03-07 18:25:53 -0500

    Remove destructor syntax because I'm out of ideas

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,29 +1,25 @@
-import {useEffect, useState, useRef} from "react";
+import {useEffect, useState} from "react";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 
 export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false);
     const [checkingStatus, setCheckingStatus] = useState(true);
 
-    const isMounted = useRef(true)
-
     useEffect(() => {
-            return () => {
-                console.log('creating auth object...')
-                const auth = getAuth();
-                onAuthStateChanged(auth, (user) => {
-                    console.log('checking auth status...')
-                    if(user){
-                        console.log('logged in')
-                        setLoggedIn(true);
-                    } else {
-                        console.log('not logged in')
-                        setLoggedIn(false);
-                    }
-                    console.log('done checking')
-                    setCheckingStatus(false);
-                })
-            };
+        console.log('creating auth object...')
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            console.log('checking auth status...')
+            if(user){
+                console.log('logged in')
+                setLoggedIn(true);
+            } else {
+                console.log('not logged in')
+                setLoggedIn(false);
+            }
+            console.log('done checking')
+            setCheckingStatus(false);
+        })
     }, []);
 
     return {loggedIn, checkingStatus}
